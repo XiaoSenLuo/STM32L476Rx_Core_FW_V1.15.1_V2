a07c58e650716d4a13c238e2c3de0953d7e8b408
xiaosenluo <xiaosenluo@yandex.com>
2022-06-03 09:53:09 +0800

    GPS 板级支持 优化

--- Core/Src/bsp_gps.c
+++ Core/Src/bsp_gps.c
@@ -27,12 +27,12 @@ void gps_pps_isr_install(GPIO_TypeDef * ppsPort, int32_t ppsPin, isr_function_ha
 
     ll_gpio_exti_isr_install(gpsPPSPin, fn, ctx);
 
-    HAL_NVIC_SetPriority(gpsPPSIRQn, 0, 0);
+    HAL_NVIC_SetPriority(gpsPPSIRQn, 1, 0);
     HAL_NVIC_EnableIRQ(gpsPPSIRQn);
 }
 
 void gps_pps_isr_uninstall(void){
-    int gpio_num = -1, irqn = -255;
+
     GPIO_InitTypeDef GPIO_InitStructure = {
             .Alternate = 0,
             .Mode = GPIO_MODE_ANALOG,
@@ -58,4 +58,36 @@ void gps_pps_irq_disable(void){
 void gps_pps_irq_enable(void){
     if((gpsPPSPort == NULL) || (gpsPPSPin < 0)) return;
     HAL_NVIC_DisableIRQ(gpsPPSIRQn);
+}
+
+#include "usart.h"
+#include "string.h"
+#include "stdarg.h"
+
+static void config_string(char* ptr, size_t cnt, const char* format, ...){
+    uint8_t cs, n;
+    va_list arp;
+
+    va_start(arp, format);
+    memset(ptr, 0, cnt);
+    n = vsnprintf(ptr, cnt, format, arp);
+    va_end(arp);
+    cs = minmea_checksum(ptr);
+    snprintf(&ptr[n], (cnt - n), "%X\r\n", cs);
+}
+
+
+void gps_config_output(const gps_ctrl_output_t *out){
+    if(out == NULL) return;
+    char pcas[64] = {'\0'}, cs = 0, n = 0;
+
+    config_string(pcas, sizeof(pcas), "$PCAS03,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,,,%d,%d,,,,%d*",
+                  out->nGGA, out->nGLL, out->nGSA,
+                  out->nGSV, out->nRMC, out->nVTG,
+                  out->nZDA, out->nANT, out->nDHV,
+                  out->nLPS, out->nUTC, out->nGST,
+                  out->nTIM
+    ); // 设置输出语句, 顺序不可改变
+
+    lpuart1_write_bytes(NULL, pcas, strlen(pcas), 10000);
 }
--- Core/Src/bsp_gps.h
+++ Core/Src/bsp_gps.h
@@ -11,11 +11,50 @@
 #include "gpio.h"
 #include "minmea.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+typedef struct gps_nmea_parse_zda_s{
+    struct minmea_sentence_zda zda;
+    char sentence[48];
+    char char_num;
+    char parse;
+}gps_nmea_parse_zda_t;
 
-void gps_pps_isr_install(GPIO_TypeDef * ppsPort, int32_t ppsPin, isr_function_handle_t fn, void *ctx);
+void gps_pps_isr_install(GPIO_TypeDef * ppsPort, int32_t ppsPin, isr_function_handle_t fn, void * ctx);
+
 void gps_pps_isr_uninstall(void);
+
 void gps_pps_irq_disable(void);
+
 void gps_pps_irq_enable(void);
+
+typedef struct gps_ctrl_output{
+    uint8_t nGGA;       /** GGA 输出频率, 语句输出频率是以定位更新率为基准的, n(0~9)表示每 n次定位输出一次, 0 表示不输出该语句, 空则保持原有配置。 **/
+    uint8_t nGLL;       /** GLL 输出频率,  同 nGGA **/
+    uint8_t nGSA;       /** GSA 输出频率,  同 nGGA **/
+    uint8_t nGSV;       /** GSV 输出频率,  同 nGGA **/
+    uint8_t nRMC;
+    uint8_t nVTG;
+    uint8_t nZDA;
+    uint8_t nANT;
+    uint8_t nDHV;
+    uint8_t nLPS;
+    uint8_t res1;
+    uint8_t res2;
+    uint8_t nUTC;
+    uint8_t nGST;
+    uint8_t res3;
+    uint8_t res4;
+    uint8_t res5;
+    uint8_t nTIM;
+}gps_ctrl_output_t;
+
+void gps_config_output(const gps_ctrl_output_t * out);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //STM32L476RX_BSP_GPS_H
