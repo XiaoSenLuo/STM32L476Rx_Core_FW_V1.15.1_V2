a3ea5560ccc2c38825ebc987f479189cca77a78b
xiaosenluo <xiaosenluo@yandex.com>
2022-06-03 09:54:25 +0800

    更新

--- Core/Src/lptim.h
+++ Core/Src/lptim.h
@@ -5,4 +5,15 @@
 #ifndef STM32L476RX_LPTIM_H
 #define STM32L476RX_LPTIM_H
 
+
+
+#include "stm32l4xx_it.h"
+
+void lptim1_initialize(LPTIM_HandleTypeDef* *lptim_handle, isr_function_handle_t fn, void *ctx);
+
+void lptim1_deinitialize(LPTIM_HandleTypeDef* *lptim_handle);
+
+void lptim1_start(LPTIM_HandleTypeDef *lptim_handle, uint16_t ht, uint16_t lt, uint16_t pulse);
+void lptim1_stop(LPTIM_HandleTypeDef *lptim_handle);
+
 #endif //STM32L476RX_LPTIM_H
